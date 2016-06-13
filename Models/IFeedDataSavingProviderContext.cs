6ada4bd3724264e22d137cbb3654d376e8503b4f
Albert Hajdu <albert.hajdu@lombiq.com>
2016-06-13 18:06:10 +0200

    Adding comments
    --HG--
    branch : dev

--- Models/IFeedDataSavingProviderContext.cs
+++ Models/IFeedDataSavingProviderContext.cs
@@ -9,12 +9,24 @@ namespace Lombiq.FeedAggregator.Models
 {
     public interface IFeedDataSavingProviderContext
     {
+        /// <summary>
+        /// The corresponding FeedSyncProfilePart.
+        /// </summary>
         FeedSyncProfilePart FeedSyncProfilePart { get; }
 
+        /// <summary>
+        /// The data what will be saved.
+        /// </summary>
         string Data { get; }
 
+        /// <summary>
+        /// The corresponding mapping.
+        /// </summary>
         Mapping Mapping { get; }
 
+        /// <summary>
+        /// The content on the data will be saved.
+        /// </summary>
         IContent Content { get; }
     }
 }
