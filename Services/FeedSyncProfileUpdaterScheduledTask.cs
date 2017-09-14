05307ea5025b60fd5c5aee975a184a071f082ea7
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-09-14 15:29:58 +0200

    Preventing endless per minute retry of a failing feed sync
    --HG--
    branch : dev

--- Services/FeedSyncProfileUpdaterScheduledTask.cs
+++ Services/FeedSyncProfileUpdaterScheduledTask.cs
@@ -16,6 +16,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
+using Orchard.Exceptions;
 
 namespace Lombiq.FeedAggregator.Services
 {
@@ -60,133 +61,145 @@ namespace Lombiq.FeedAggregator.Services
             if (feedSyncProfileContentItem == null ||
                 feedSyncProfileContentItem.ContentType != ContentTypes.FeedSyncProfile) return;
 
-            var feedSyncProfilePart = feedSyncProfileContentItem.As<FeedSyncProfilePart>();
-
-            var newEntries = new List<XElement>();
-            foreach (var feedEntryExtractorProvider in _feedEntryExtractors)
+            try
             {
-                var extractedEntries = feedEntryExtractorProvider
-                    .GetNewValidEntries(feedSyncProfilePart, feedSyncProfilePart.FeedType);
+                var feedSyncProfilePart = feedSyncProfileContentItem.As<FeedSyncProfilePart>();
 
-                newEntries.AddRange(extractedEntries);
-            }
-
-            foreach (var newEntry in newEntries)
-            {
-                // Persisting must happen only if at least one successful mapping saving happened.
-                var contentItemShouldBePersisted = false;
-
-                var feedItemIdNode = newEntry.GetDescendantNodeByName(feedSyncProfilePart.FeedItemIdType);
-                if (feedItemIdNode == null) continue;
-                var feedItemId = feedItemIdNode.Value;
-                var feedItemModificationDateNode = newEntry.GetDescendantNodeByName(feedSyncProfilePart.FeedItemModificationDateType);
-                var feedItemModificationDate = new DateTime();
-                if (feedItemModificationDateNode == null ||
-                    !DateTimeHelper.TryParseDateTime(feedItemModificationDateNode.Value, out feedItemModificationDate)) continue;
-
-                var feedSyncProfileItem = _contentManager
-                        .Query(feedSyncProfilePart.ContentType)
-                        .Where<FeedSyncProfileItemPartRecord>(record => record.FeedItemId == feedItemId)
-                        .Slice(1)
-                        .FirstOrDefault();
-                if (feedSyncProfileItem == null)
+                var newEntries = new List<XElement>();
+                foreach (var feedEntryExtractorProvider in _feedEntryExtractors)
                 {
-                    feedSyncProfileItem = _contentManager.New(feedSyncProfilePart.ContentType);
-                    _contentManager.Create(feedSyncProfileItem, VersionOptions.Draft);
+                    var extractedEntries = feedEntryExtractorProvider
+                        .GetNewValidEntries(feedSyncProfilePart, feedSyncProfilePart.FeedType);
+
+                    newEntries.AddRange(extractedEntries);
                 }
 
-                foreach (var mapping in feedSyncProfilePart.Mappings)
+                foreach (var newEntry in newEntries)
                 {
-                    var feedEntryData = new List<string>();
-                    var splitFeedMapping = mapping.FeedMapping.Split('.');
-                    List<XElement> selectedNodesInFeedEntry;
-                    if (!newEntry.TryGetNodesByPath(splitFeedMapping[0], out selectedNodesInFeedEntry))
-                        continue;
-                    // If it is an attribute mapping.
-                    if (splitFeedMapping.Length > 1)
-                    {
-                        var feedEntryAttribute = selectedNodesInFeedEntry.First().Attribute(splitFeedMapping[1]);
-                        if (feedEntryAttribute == null) continue;
+                    // Persisting must happen only if at least one successful mapping saving happened.
+                    var contentItemShouldBePersisted = false;
 
-                        feedEntryData.Add(feedEntryAttribute.Value);
+                    var feedItemIdNode = newEntry.GetDescendantNodeByName(feedSyncProfilePart.FeedItemIdType);
+                    if (feedItemIdNode == null) continue;
+                    var feedItemId = feedItemIdNode.Value;
+                    var feedItemModificationDateNode = newEntry.GetDescendantNodeByName(feedSyncProfilePart.FeedItemModificationDateType);
+                    var feedItemModificationDate = new DateTime();
+                    if (feedItemModificationDateNode == null ||
+                        !DateTimeHelper.TryParseDateTime(feedItemModificationDateNode.Value, out feedItemModificationDate)) continue;
+
+                    var feedSyncProfileItem = _contentManager
+                            .Query(feedSyncProfilePart.ContentType)
+                            .Where<FeedSyncProfileItemPartRecord>(record => record.FeedItemId == feedItemId)
+                            .Slice(1)
+                            .FirstOrDefault();
+                    if (feedSyncProfileItem == null)
+                    {
+                        feedSyncProfileItem = _contentManager.New(feedSyncProfilePart.ContentType);
+                        _contentManager.Create(feedSyncProfileItem, VersionOptions.Draft);
                     }
-                    // If it is node mapping,
+
+                    foreach (var mapping in feedSyncProfilePart.Mappings)
+                    {
+                        var feedEntryData = new List<string>();
+                        var splitFeedMapping = mapping.FeedMapping.Split('.');
+                        List<XElement> selectedNodesInFeedEntry;
+                        if (!newEntry.TryGetNodesByPath(splitFeedMapping[0], out selectedNodesInFeedEntry))
+                            continue;
+                        // If it is an attribute mapping.
+                        if (splitFeedMapping.Length > 1)
+                        {
+                            var feedEntryAttribute = selectedNodesInFeedEntry.First().Attribute(splitFeedMapping[1]);
+                            if (feedEntryAttribute == null) continue;
+
+                            feedEntryData.Add(feedEntryAttribute.Value);
+                        }
+                        // If it is node mapping,
+                        else
+                        {
+                            // This is necessary, because sometimes the node contains another node as the actual content.
+                            // E.g. img element.
+                            feedEntryData.AddRange(
+                                selectedNodesInFeedEntry
+                                .Select(node => string.IsNullOrEmpty(node.Value)
+                                    ? string.Join("", node.Nodes().Select(x => x.ToString()))
+                                    : node.Value));
+                        }
+
+                        var successfulMappingSaving = false;
+                        foreach (var feedDataSavingProvider in _feedDataSavingProviders)
+                        {
+                            successfulMappingSaving = feedDataSavingProvider.Save(new FeedDataSavingProviderContext
+                            {
+                                FeedSyncProfilePart = feedSyncProfilePart,
+                                FeedContent = feedEntryData,
+                                Mapping = mapping,
+                                Content = feedSyncProfileItem
+                            });
+
+                            // In case of a provider saved the data continue with the next mapping.
+                            if (successfulMappingSaving)
+                            {
+                                contentItemShouldBePersisted = true;
+                                break;
+                            }
+                        }
+
+                        if (!successfulMappingSaving)
+                        {
+                            var feedSyncProfileTitlePart = feedSyncProfileContentItem.As<TitlePart>();
+                            Logger.Error(string.Format(
+                                "No suitable provider for the selected mapping ({0} to {1}). This can be due to a content type definition change. Please edit and save the corresponding (Title: {2}) FeedSyncProfile.",
+                                mapping.FeedMapping,
+                                mapping.ContentItemStorageMapping,
+                                feedSyncProfileTitlePart == null ? "unknown" : feedSyncProfileTitlePart.Title));
+                        }
+                    }
+
+                    if (contentItemShouldBePersisted)
+                    {
+                        // We publish it only if at least one mapping saving was successful.
+                        // Also this is the time when we want to store the last creation date
+                        // on the FeedSyncProfilePart and set the FeedItemId if it not set yet.
+                        var feedSyncProfileItemPart = feedSyncProfileItem.As<FeedSyncProfileItemPart>();
+                        if (string.IsNullOrEmpty(feedSyncProfileItemPart.FeedItemId))
+                        {
+                            feedSyncProfileItemPart.FeedItemId = feedItemId;
+                        }
+                        feedSyncProfileItemPart.FeedSyncProfileId = feedSyncProfilePart.ContentItem.Id;
+                        feedSyncProfilePart.LatestCreatedItemModificationDate = feedItemModificationDate;
+                        // Setting the content item's container.
+                        var container = feedSyncProfilePart.Container.ContentItems.Any()
+                            ? feedSyncProfilePart.Container.ContentItems.First()
+                            : null;
+                        if (container != null)
+                        {
+                            var commonPart = feedSyncProfileItem.As<CommonPart>();
+                            if (commonPart != null)
+                            {
+                                commonPart.Container = container;
+                            }
+                        }
+
+                        _contentManager.Publish(feedSyncProfileItem);
+                    }
                     else
                     {
-                        // This is necessary, because sometimes the node contains another node as the actual content.
-                        // E.g. img element.
-                        feedEntryData.AddRange(
-                            selectedNodesInFeedEntry
-                            .Select(node => string.IsNullOrEmpty(node.Value)
-                                ? string.Join("", node.Nodes().Select(x => x.ToString()))
-                                : node.Value));
-                    }
-
-                    var successfulMappingSaving = false;
-                    foreach (var feedDataSavingProvider in _feedDataSavingProviders)
-                    {
-                        successfulMappingSaving = feedDataSavingProvider.Save(new FeedDataSavingProviderContext
-                        {
-                            FeedSyncProfilePart = feedSyncProfilePart,
-                            FeedContent = feedEntryData,
-                            Mapping = mapping,
-                            Content = feedSyncProfileItem
-                        });
-
-                        // In case of a provider saved the data continue with the next mapping.
-                        if (successfulMappingSaving)
-                        {
-                            contentItemShouldBePersisted = true;
-                            break;
-                        }
-                    }
-
-                    if (!successfulMappingSaving)
-                    {
-                        var feedSyncProfileTitlePart = feedSyncProfileContentItem.As<TitlePart>();
-                        Logger.Error(string.Format(
-                            "No suitable provider for the selected mapping ({0} to {1}). This can be due to a content type definition change. Please edit and save the corresponding (Title: {2}) FeedSyncProfile.",
-                            mapping.FeedMapping,
-                            mapping.ContentItemStorageMapping,
-                            feedSyncProfileTitlePart == null ? "unknown" : feedSyncProfileTitlePart.Title));
+                        _contentManager.Remove(feedSyncProfileItem);
                     }
                 }
 
-                if (contentItemShouldBePersisted)
-                {
-                    // We publish it only if at least one mapping saving was successful.
-                    // Also this is the time when we want to store the last creation date
-                    // on the FeedSyncProfilePart and set the FeedItemId if it not set yet.
-                    var feedSyncProfileItemPart = feedSyncProfileItem.As<FeedSyncProfileItemPart>();
-                    if (string.IsNullOrEmpty(feedSyncProfileItemPart.FeedItemId))
-                    {
-                        feedSyncProfileItemPart.FeedItemId = feedItemId;
-                    }
-                    feedSyncProfileItemPart.FeedSyncProfileId = feedSyncProfilePart.ContentItem.Id;
-                    feedSyncProfilePart.LatestCreatedItemModificationDate = feedItemModificationDate;
-                    // Setting the content item's container.
-                    var container = feedSyncProfilePart.Container.ContentItems.Any()
-                        ? feedSyncProfilePart.Container.ContentItems.First()
-                        : null;
-                    if (container != null)
-                    {
-                        var commonPart = feedSyncProfileItem.As<CommonPart>();
-                        if (commonPart != null)
-                        {
-                            commonPart.Container = container;
-                        }
-                    }
-
-                    _contentManager.Publish(feedSyncProfileItem);
-                }
-                else
-                {
-                    _contentManager.Remove(feedSyncProfileItem);
-                }
+                feedSyncProfilePart.SuccesfulInit = true;
+                feedSyncProfilePart.LatestCreatedItemModificationDate = _clock.UtcNow;
             }
+            catch (Exception ex) when (!ex.IsFatal())
+            {
+                // Swallowing all non-fatal exceptions so if a sync fails it won't be retried every minute. That would
+                // happen if the exception would bubble up and thus also prevent the removal of the current task entry.
 
-            feedSyncProfilePart.SuccesfulInit = true;
-            feedSyncProfilePart.LatestCreatedItemModificationDate = _clock.UtcNow;
+                Logger.Error
+                    (ex, 
+                    "Error during processing the FeedSyncProfile with the ID " + feedSyncProfileContentItem.Id + ".");
+            }
         }
 
         public void Activated()
